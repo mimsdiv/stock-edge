3709ce96d0cf3b097769c447707c081452bf21e1
Anmol Khurana <anmolkhurana490@gmail.com>
2025-06-26 15:18:39 +0530

    code corrected

--- main.js
+++ main.js
@@ -106,7 +106,7 @@ const main = async () => {
 
         // Launch Puppeteer
         const browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
